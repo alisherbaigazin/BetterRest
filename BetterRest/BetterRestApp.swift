5add7bfa408eb44903aec3c61dacb93ab0579b6f
alisherbaigazin <alisherbaigazin@gmail.com>
2022-06-04 21:26:09 +0600

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Alisher Baigazin on 04.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
